835eef3650040988b5fb2c70e666a47d995d63c9
= <=>
2023-09-20 19:41:42 -0400

    parte4ProjetoFinal

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    """Function to get AI response from url"""
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
     if response.status_code == 200:
+        """Set object if response is successfull"""
         emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
         dominant_emotion = max(emotion_predictions, key=lambda k: emotion_predictions[k])
         result = {
@@ -19,6 +21,7 @@ def emotion_detector(text_to_analyse):
             'dominant_emotion': dominant_emotion
         }
     elif response.status_code == 400:
+        """Set None object if response is unsuccessfull"""
         result = {
             'anger': None,
             'disgust': None,
--- server.py
+++ server.py
@@ -1,28 +1,36 @@
+"""
+This is a Flask server for the Emotion Detector application.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
+
 app = Flask("Emotion Detector")
+"""Set app Name and Starts Flask"""
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """Starts request and retuen response"""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-    else:
-        result = {
-            'anger': response['anger'],
-            'disgust': response['disgust'],
-            'fear': response['fear'],
-            'joy': response['joy'],
-            'sadness': response['sadness']
-        }
-        return "For the given statement, the system response is {}. The dominant emotion is {}.".format(str(result).replace('{', '').replace('}', ''), response['dominant_emotion'])
+    result = {
+        'anger': response['anger'],
+        'disgust': response['disgust'],
+        'fear': response['fear'],
+        'joy': response['joy'],
+        'sadness': response['sadness']
+    }
+    str_result = str(result).replace('{', '').replace('}', '')
+    emotion = response['dominant_emotion']
+    r_string = "For the given statement, the system response is {}. The dominant emotion is {}."
+    return r_string.format(str_result, emotion)
 
 @app.route("/")
 def render_index_page():
+    """Render Index"""
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
-
