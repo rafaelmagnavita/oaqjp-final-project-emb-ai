1d6d7f570e0250109970e06b8589dfdbf6ee0498
= <=>
2023-09-20 16:46:25 -0400

    parte4ProjetoFinal

--- server.py
+++ server.py
@@ -12,15 +12,14 @@ def emot_detector():
         'disgust': response['disgust'],
         'fear': response['fear'],
         'joy': response['joy'],
-        'sadness': response['sadness'],
-        'dominant_emotion': response['dominant_emotion']
+        'sadness': response['sadness']
     }
-    return "For the given statement, the system response is: " + result
+    return "For the given statement, the system response is {}. The dominant emotion is {}.".format(str(result).replace('{', '').replace('}', ''), response['dominant_emotion'])
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
 
