f0a58943ed8c71f469b59957368232835d58ecff
= <=>
2023-09-20 17:04:10 -0400

    parte4ProjetoFinal

--- server.py
+++ server.py
@@ -7,14 +7,17 @@ app = Flask("Emotion Detector")
 def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    result = {
-        'anger': response['anger'],
-        'disgust': response['disgust'],
-        'fear': response['fear'],
-        'joy': response['joy'],
-        'sadness': response['sadness']
-    }
-    return "For the given statement, the system response is {}. The dominant emotion is {}.".format(str(result).replace('{', '').replace('}', ''), response['dominant_emotion'])
+    if response is None:
+        return "Invalid input ! Try again."
+    else
+        result = {
+            'anger': response['anger'],
+            'disgust': response['disgust'],
+            'fear': response['fear'],
+            'joy': response['joy'],
+            'sadness': response['sadness']
+        }
+        return "For the given statement, the system response is {}. The dominant emotion is {}.".format(str(result).replace('{', '').replace('}', ''), response['dominant_emotion'])
 
 @app.route("/")
 def render_index_page():
